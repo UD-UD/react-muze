4389ce220033de0cfb0665db09a64751d817de8a
Ujjal Kumar Dutta <duttaujjal143@gmail.com>
2018-10-18 20:18:58 +0530

    gp pages

---
+++ precache-manifest.f7ad481705a496b436b002cc186a0a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5dfb49d193d7a75dea10",
+    "url": "/static/js/main.5dfb49d1.chunk.js"
+  },
+  {
+    "revision": "0b903c68968981be352c",
+    "url": "/static/js/1.0b903c68.chunk.js"
+  },
+  {
+    "revision": "5dfb49d193d7a75dea10",
+    "url": "/static/css/main.ef976d30.chunk.css"
+  },
+  {
+    "revision": "0b903c68968981be352c",
+    "url": "/static/css/1.4ec6a1fc.chunk.css"
+  },
+  {
+    "revision": "2983a5da23cb083d59de14bae18949ef",
+    "url": "/index.html"
+  }
+];
